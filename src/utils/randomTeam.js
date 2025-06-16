7e22838f4289099d9d42672103c2dee5ea7bce1f
RomaanDeRose <romandr37@gmail.com>
2025-06-16 15:39:39 -0300

    Funciones de utilidad para manejar los equipos

---
+++ src/utils/randomTeam.js
@@ -0,0 +1,30 @@
+const randomTeam = (teams) => {
+  const randomTeams = [];
+  for (let i = 0; i < 10; i++) {
+    const randomIndex = Math.floor(Math.random() * teams.length);
+    const randomTeam = teams[randomIndex];
+
+    let isDuplicate = randomTeams.some((team) => team.id === randomTeam.id);
+    let isInArray = false;
+
+    while (!isInArray) {
+      if (!isDuplicate) {
+        randomTeams.push(randomTeam);
+        isInArray = true;
+      } else {
+        const newRandomIndex = Math.floor(Math.random() * teams.length);
+        const newRandomTeam = teams[newRandomIndex];
+
+        isDuplicate = randomTeams.some((team) => team.id === newRandomTeam.id);
+        if (!isDuplicate) {
+          randomTeams.push(newRandomTeam);
+          isInArray = true;
+        }
+      }
+    }
+  }
+
+  return randomTeams;
+};
+
+export { randomTeam };
